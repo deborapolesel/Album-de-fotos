b46e51382d7afede6dc992decd9628e700294284
Débora Polesel <deborahpolesel@hotmail.com>
2020-06-24 09:11:28 -0300

    Upload Arquivo Javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+// Declaração das constantes e variáveis do documento:
+
+const botaoVoltar = document.querySelector('.botaoAnterior');
+
+const botaoAvançar = document.querySelector('.botaoPosterior');
+
+const imagens_div = document.querySelector('.imagens');
+
+let contador = 0;
+
+// Criando eventos para os botões:
+
+botaoAvançar.addEventListener('click', avancarFoto);
+botaoVoltar.addEventListener('click', voltarFoto);
+
+
+// Criar as funções para as mudanças de imagens:
+
+// Funçao AVANÇAR FOTO:
+function avancarFoto(){
+
+    if(contador === 5){
+        contador = 0;
+    }
+
+    contador++;
+
+    imagens_div.style.backgroundImage = `url(img/dog${contador}.png)`
+}
+
+// Funçao VOLTAR FOTO:
+function voltarFoto(){
+
+    if(contador === 1){
+        contador = 6;
+    }
+
+    contador--;
+    
+    imagens_div.style.backgroundImage = `url(img/dog${contador}.png)`
+}
+
